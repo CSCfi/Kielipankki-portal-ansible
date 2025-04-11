1026e43cde69bfaed2cacdba72bd742d26c43d17
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2025-04-11 12:18:11 +0000

    KP-9064 Update jquery of Kielipankki theme

--- roles/wordpress/files/kielipankki/header.php
+++ roles/wordpress/files/kielipankki/header.php
@@ -5,7 +5,7 @@
 <link href="<?php echo get_template_directory_uri(); ?>/tyylit.css" rel="stylesheet" type="text/css">
 <?php if( WP_DEBUG === true ) { echo '<link href="'.get_template_directory_uri().'/tyylit_debug.css" rel="stylesheet" type="text/css">'; } ?>
 <script src="<?php echo get_template_directory_uri(); ?>/js/modernizr_2_8_3_custom.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.3.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.7.1.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/SimpleCssParser.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/polycalc.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/koodit.js"></script>
