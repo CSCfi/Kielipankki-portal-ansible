d93f731634645b3ee461fcf8ef7e4ceb581a670e
Sam Hardwick <sam.hardwick@csc.fi>
2025-12-02 14:58:04 +0200

    KP-10346 Fix child pages listing function
    It previously listed children of the parent, but apparently we really
    want children of the current page.

--- roles/wordpress/files/kielipankki/functions.php
+++ roles/wordpress/files/kielipankki/functions.php
@@ -194,22 +194,9 @@ function custom_theme_setup() {
 }
 
 function wpb_list_child_pages() {
-
 global $post;
-
-if ( is_page() && $post->post_parent )
-
-   $childpages = wp_list_pages( 'sort_column=post_title&title_li=&child_of=' . $post->post_parent . '&echo=0' );
-else
-	$childpages = wp_list_pages( 'sort_column=post_title&title_li=&child_of=' . $post->ID . '&echo=0' );
-
-if ( $childpages ) {
-
-   $string = '<ul>' . $childpages . '</ul>';
-}
-
-return $string;
-
+$childpages = wp_list_pages( 'sort_column=post_title&title_li=&child_of=' . $post->ID . '&echo=0' );
+return '<ul>' . $childpages . '</ul>';
 }
 
 add_shortcode('wpb_childpages', 'wpb_list_child_pages');
